998318a7f2da05b44f69f57f25537d3fdcb6f247
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-09-17 16:17:29 -0500

    Dom Manegement

--- js practices/DOM management/script.js
+++ js practices/DOM management/script.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+const h1 = document.querySelector('h1');
+const paragraph = document.querySelector('.parrafo');
+const ConId = document.querySelector('P2');
+const input = document.querySelector('input');
+
+console.log(input.value);
+
+console.log({
+    h1,
+    paragraph,
+    ConId,
+    input,
+});
