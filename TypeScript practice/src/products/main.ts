df5cd8b71b653a282636cade8772dad4eab9af3a
Alejandro Ching <alejandroching2004@hotmail.com>
2022-11-26 23:40:01 -0500

    Finishing

--- TypeScript practice/src/products/main.ts
+++ TypeScript practice/src/products/main.ts
@@ -1,16 +1,19 @@
 import { addProduct, calcStock, products } from './product.service';
 
 addProduct({
-  title: 'Chesse',
+  name: 'Chesse',
   createAt: new Date(),
   stock: 50,
   size: "XL"
 })
 
 addProduct({
-  title: 'Rice',
+  name: 'Rice',
   createAt: new Date(),
   stock: 60,
   size: "S"
 })
 console.log(products);
+const total = calcStock();
+
+console.log(total);
--- TypeScript practice/src/products/product.model.ts
+++ TypeScript practice/src/products/product.model.ts
@@ -1,6 +1,6 @@
 export type Size = 'S' | 'M' | 'L' | 'XL'
 export type Product = { //This is an object as a type
-    title: string,
+    name: string,
     createAt: Date,
     stock: number,
     size?: Size
